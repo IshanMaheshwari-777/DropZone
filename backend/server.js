88c61f3906895ac3df65a3c79452378b898ba63c
IshanMaheshwari-777 <ishan77maheshwari@gmail.com>
2026-01-08 00:20:34 +0530

    fixed patch error

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(morgan('dev'));
 app.use(cors({
     origin: ["https://drop-zone-beta.vercel.app", "http://localhost:3000"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 })); 
 app.use(express.json());
