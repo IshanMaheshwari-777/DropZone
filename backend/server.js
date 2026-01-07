30062c38f4e7532de55aee69bf2fa7f2ccbba4d9
IshanMaheshwari-777 <ishan77maheshwari@gmail.com>
2026-01-08 00:01:15 +0530

    fixed cors  error

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors({
-    origin: ["https://drop-zone-beta.vercel.app/", "http://localhost:3000"],
+    origin: ["https://drop-zone-beta.vercel.app", "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
